22842c05a175e06af776e6b4a053fd297ab5167c
Lucas Martinelli <martinellilucas8@gmail.com>
2023-03-07 18:51:47 -0300

    replantie el disenio de las cards agregandoleuna transicion

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,15 +3,19 @@ import style from "./Card.module.css";
 export default function Card({ name, species, gender, image, onClose }) {
   return (
     <div className={style.container}>
-      <div className={style.descriptionContainer}>
-        <h2 className={style.description}>~ Name: {name}</h2>
-        <h2 className={style.description}>~ Species: {species}</h2>
-        <h2 className={style.description}>~ Gender: {gender}</h2>
+      <div className={style.card}>
+        <div className={style.image}>
+          <img className={style.img} src={image} alt="" />
+        </div>
+        <div className={style.content}>
+          <button className={style.button} onClick={onClose}>
+            X
+          </button>
+          <h2 className={style.description}>~ NAME: {name}</h2>
+          <h2 className={style.description}>~ SPECIE: {species}</h2>
+          <h2 className={style.description}>~ GENDER: {gender}</h2>
+        </div>
       </div>
-      <img className={style.image} src={image} alt="" />
-      <button className={style.button} onClick={onClose}>
-        X
-      </button>
     </div>
   );
 }
