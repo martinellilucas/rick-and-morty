ebf75da555b034fe48757eba14f1e821d43a821b
Lucas Martinelli <martinellilucas8@gmail.com>
2023-03-30 13:31:37 -0300

    responsive mejorada

--- front/src/index.js
+++ front/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
